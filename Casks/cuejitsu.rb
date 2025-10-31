6cd2cd0035587b0a795b8241dfae0e9151574d8d
Chandler Anderson <chanderson@mac-mini.local>
2025-10-31 17:40:14 -0400

    Add Cuejitsu cask formula

---
+++ Casks/cuejitsu.rb
@@ -0,0 +1,27 @@
+cask "cuejitsu" do
+  version "1.0.0"
+  sha256 :no_check # Will be updated with actual SHA256 after first release
+
+  url "https://github.com/zenibako/cuejitsu/releases/download/v#{version}/Cuejitsu-#{version}.dmg"
+  name "Cuejitsu"
+  desc "Menu bar app for controlling QLab from screenwriting software"
+  homepage "https://github.com/zenibako/cuejitsu"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Cuejitsu.app"
+
+  # No quarantine since app is unsigned
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/Cuejitsu.app"]
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.zenibako.cuejitsu.plist",
+    "~/Library/Saved Application State/com.zenibako.cuejitsu.savedState",
+  ]
+end
